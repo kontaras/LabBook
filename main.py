3df282f35c7311be1eac69d4ac9269779a6d1495
Konstantin Naryshkin <knary@users.noreply.github.com>
2019-02-06 20:38:00 -0500

    Added an output template

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import markdown
-from bottle import route, run, HTTPError
+from bottle import route, run, HTTPError, view
 import io
 from os import path
 
 
 @route('/')
 def index():
-    return getFile("README.md")
+    return getFile("/")
 
 
 @route('/<doc:path>')
+@view('page')
 def getFile(doc):
     siteDir = path.abspath("site")
     fl = path.normpath(path.join(siteDir, doc + ".md"))
     if not fl.startswith(siteDir) or not path.exists(fl):
         raise HTTPError(status=404)
+    
+    page={}
+    page["path"] = doc
+    
     with io.BytesIO() as output, open(fl, "rb") as code:
         markdown.markdownFromFile(input=code, output=output)
-        return output.getvalue()
+        page["contents"] = output.getvalue()
+        return page
 
 
 run(host='localhost', port=8080)
