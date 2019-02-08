7b1576c567ab4603cc339b37ea7ff19ff02b00a3
Konstantin Naryshkin <knary@users.noreply.github.com>
2019-02-07 21:35:29 -0500

    Added more path inference

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import markdown
-from bottle import route, run, HTTPError, view
+from bottle import route, run, view, abort
 import io
 from os import path
 
+siteDir = path.abspath("site")
 
 @route('/')
 def index():
@@ -12,18 +13,23 @@ def index():
 @route('/<doc:path>')
 @view('page')
 def getFile(doc):
-    siteDir = path.abspath("site")
-    fl = path.normpath(path.join(siteDir, doc + ".md"))
-    if not fl.startswith(siteDir) or not path.exists(fl):
-        raise HTTPError(status=404)
+    itemPath = path.normpath(path.join(siteDir, doc.strip("/\\")))
+    pathBits = []
+    
+    while not path.isfile(itemPath + ".md"):
+        if not itemPath.startswith(siteDir):
+            abort(404, "Invalid article")
+        itemPath, pathBit = path.split(itemPath)
+        pathBits.append(pathBit)
+        print(itemPath, pathBits)
     
     page={}
     page["path"] = doc
     
-    with io.BytesIO() as output, open(fl, "rb") as code:
+    with io.BytesIO() as output, open(itemPath + ".md", "rb") as code:
         markdown.markdownFromFile(input=code, output=output)
         page["contents"] = output.getvalue()
         return page
 
 
-run(host='localhost', port=8080)
+run(host='localhost', port=8080, reloader=True)
