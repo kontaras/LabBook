44f8dd6a9037ff863140120ca2248b226d1470a9
Konstantin Tarashchanskiy <knary@users.noreply.github.com>
2019-09-10 22:42:41 -0400

    Use envorinment variables actually used by Heroku

--- main.py
+++ main.py
@@ -97,7 +97,4 @@ def get_markdown_content(pagePath):
 
 
 if __name__ == "__main__":
-    if os.environ.get('APP_LOCATION') == 'heroku':
-        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    else:
-        app.run(host='localhost', port=8080, reloader=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
