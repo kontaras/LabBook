1039384e4aff5666eab5dd4e666c31ace34d98fb
Konstantin Naryshkin <knary@users.noreply.github.com>
2019-09-26 22:52:15 -0400

    Added more tests for get_sub_pages and sorted its output

--- unit_tests/test_get_sub_pages.py
+++ unit_tests/test_get_sub_pages.py
@@ -1,6 +1,7 @@
 import unittest
 import app
 import tempfile
+import os
 
 
 class TestGetSubPages(unittest.TestCase):
@@ -11,6 +12,54 @@ class TestGetSubPages(unittest.TestCase):
     def test_file(self):
         with tempfile.NamedTemporaryFile(mode="x") as tmp_file:
             self.assertListEqual(app.get_sub_pages(tmp_file.name), [])
+
+    def test_dir_only_index(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            open(os.path.join(tmp_dir, "index.md"), "x").close()
+            self.assertListEqual(app.get_sub_pages(tmp_dir), [])
+
+    def test_dir_non_md(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            open(os.path.join(tmp_dir, "valid.txt"), "x").close()
+            self.assertListEqual(app.get_sub_pages(tmp_dir), [])
+
+    def test_dir_with_file(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            open(os.path.join(tmp_dir, "pindex.md"), "x").close()
+            self.assertListEqual(app.get_sub_pages(tmp_dir), ["pindex"])
+
+    def test_dir_with_files(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            open(os.path.join(tmp_dir, "pindex.md"), "x").close()
+            open(os.path.join(tmp_dir, "pindex2.md"), "x").close()
+            self.assertListEqual(app.get_sub_pages(tmp_dir), ["pindex",
+                                                              "pindex2"])
+
+    def test_dir_with_sub_dir(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            os.mkdir(os.path.join(tmp_dir, "pindex"))
+            self.assertListEqual(app.get_sub_pages(tmp_dir), ["pindex"])
+
+    def test_dir_with_sub_dirs(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            os.mkdir(os.path.join(tmp_dir, "pindex"))
+            os.mkdir(os.path.join(tmp_dir, "pindex2"))
+            self.assertListEqual(app.get_sub_pages(tmp_dir), ["pindex",
+                                                              "pindex2"])
+
+    def test_dir_with_dir_and_file(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            os.mkdir(os.path.join(tmp_dir, "dir"))
+            open(os.path.join(tmp_dir, "pindex.md"), "x").close()
+            self.assertListEqual(app.get_sub_pages(tmp_dir), ["dir", "pindex"])
+
+    def test_dir_with_dir_file_and_junk(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            os.mkdir(os.path.join(tmp_dir, "dir"))
+            open(os.path.join(tmp_dir, "pindex.md"), "x").close()
+            open(os.path.join(tmp_dir, "valid.txt"), "x").close()
+            open(os.path.join(tmp_dir, "index.md"), "x").close()
+            self.assertListEqual(app.get_sub_pages(tmp_dir), ["dir", "pindex"])
 
 
 if __name__ == "__main__":
