ce40d8d366e0752624c5edb3e80bd731c5737d7f
Konstantin Tarashchanskiy <knary@users.noreply.github.com>
2019-09-27 22:49:04 -0400

    Try to fix Windows

--- unit_tests/test_get_markdown_content.py
+++ unit_tests/test_get_markdown_content.py
@@ -1,16 +1,21 @@
 import unittest
 import tempfile
+import os
 from app import get_markdown_content
 
 
 class TestGetMarkdownContents(unittest.TestCase):
     def test_markdown(self):
-        with tempfile.NamedTemporaryFile() as fd:
-            fd.write(b"Heading")
-            fd.flush()
-            print(fd.name)
-            content = get_markdown_content(fd.name)
+        fd = tempfile.mkstemp()
+        try:
+            with open(fd[1], "w") as writable:
+                writable.write("Heading")
+                writable.flush()
+            content = get_markdown_content(fd[1])
             self.assertEqual(content, b"<p>Heading</p>")
+        finally:
+            os.close(fd[0])
+            os.unlink(fd[1])
 
 
 if __name__ == '__main__':
