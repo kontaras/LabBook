e8561a5b2e37e5e285503a2cd66064f1f9d65eba
Konstantin Naryshkin <knary@users.noreply.github.com>
2020-09-23 20:11:24 -0400

    Added integration tests for index pages

---
+++ integ_tests/test_index_page.py
@@ -0,0 +1,23 @@
+'''
+test that index pages are properly found
+'''
+import unittest
+from utils import run_page
+
+
+class Test(unittest.TestCase):
+
+    def test_root(self):
+        "Test that the root path"
+        parts = run_page({"index": "the root index"}, "")
+        self.assertEqual(parts["contents"], "<p>the root index</p>")
+
+    def test_dir(self):
+        "Test directory index"
+        parts = run_page({"index": "the root index",
+                          "dir\\index": "the dir index"}, "dir")
+        self.assertEqual(parts["contents"], "<p>the dir index</p>")
+
+
+if __name__ == "__main__":
+    unittest.main()
